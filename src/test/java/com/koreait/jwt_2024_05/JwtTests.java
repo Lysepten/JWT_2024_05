0397c65252c87624374cb1b09b37e67f3bb8afa6
Lysepten <tlstlsrbrb11@gmail.com>
2024-05-14 09:47:28 +0900

    시크릿키 원문으로 HMAC 암호화 방식에 맞는 SecretKey 객체 만들기, SecretKey 객체는 jwtProvider 객체를 통해서 얻는다

--- src/test/java/com/koreait/jwt_2024_05/JwtTests.java
+++ src/test/java/com/koreait/jwt_2024_05/JwtTests.java
@@ -1,21 +1,58 @@
 package com.koreait.jwt_2024_05;
 
+import com.koreait.jwt_2024_05.base.jwt.JwtProvider;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTests {
-  @Value("${custom.jwt.secretKey}")
-  private String secretKey;
+    @Autowired
+    private JwtProvider jwtProvider;
 
-  @Test
-  @DisplayName("secretKey가 존재해야 함.")
-  void contextLoads() {
-    assertThat(secretKey).isNotNull();
-  }
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyPlain;
 
+    @Test
+    @DisplayName("secretKey가 존재해야 함.")
+    void t1() {
+        assertThat(secretKeyPlain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("secretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
+    void t2() {
+        // 키를 Base64 인코딩 한다.
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+
+        // Base64 인코딩 된 키를 이용해서 SecretKey 객체를 만든다.
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
+        assertThat(secretKeyPlain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("JwtProvider 객체로 SecretKey 객체 생성")
+    void t3() {
+        SecretKey secretKey = jwtProvider.getSecretKey();
+
+        assertThat(secretKeyPlain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("SecretKey 객체는 단 한번만 생생 되어야 함.")
+    void t4() {
+        SecretKey secretKey1 = jwtProvider.getSecretKey();
+        SecretKey secretKey2 = jwtProvider.getSecretKey();
+
+        assertThat(secretKey1 == secretKey2).isTrue();
+    }
 }
